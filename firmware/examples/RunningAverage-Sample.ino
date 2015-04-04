8dc8ada4c09703ac502879accdbfa9ed48001391
lchoate <lucasoptura@gmail.com>
2015-04-04 01:02:10 -0700

    Initial Commit

---
+++ firmware/examples/RunningAverage-Sample.ino
@@ -0,0 +1,30 @@
+#include "RunningAverage.h"
+
+RunningAverage myRA(10);
+int samples = 0;
+
+void setup(void) 
+{
+  Serial.begin(115200);
+  Serial.print("Demo RunningAverage lib - fillValue ");
+  Serial.print("Version: ");
+  Serial.println(RUNNINGAVERAGE_LIB_VERSION);
+ 
+  myRA.fillValue(100,5);
+}
+
+void loop(void) 
+{
+  long rn = random(0, 100);
+  myRA.addValue(rn/100.0);
+  samples++;
+  Serial.print("Running Average: ");
+  Serial.println(myRA.getAverage(), 4);
+  
+  if (samples == 300)
+  {
+    samples = 0;
+    myRA.fillValue(100, 10);
+  }
+  delay(100);
+}
